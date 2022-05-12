54898aeebb3cd66495be8ef9e5542bba2b97a536
prop07 <rijalmanoj07@gmail.com>
2022-05-12 15:23:44 +0545

    -

---
+++ api/serializer.py
@@ -0,0 +1,60 @@
+from rest_framework import serializers
+from .models import User, Color, Order, Product,ProductType, ProductImage, Category, CategoryProduct, Slider, OrderItem
+
+
+
+class SliderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Slider
+        fields = '__all__'
+
+
+class ColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Color
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+        
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = '__all__'
+class ProductTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductType
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    orderItem = OrderItemSerializer(many=True, read_only=True)
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    colors = ColorSerializer(many=True, read_only=True)
+    types = ProductTypeSerializer(many=True, read_only=True)
+    images = ProductImageSerializer(many=True, read_only=True)
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class CategoryProductSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+    class Meta:
+        model = CategoryProduct
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    categories =  CategoryProductSerializer(many=True, read_only=True)
+    class Meta:
+        model = Category
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import JsonResponse
+from .models import Order, OrderItem, Product,  Category, CategoryProduct, Slider
+from .serializer import OrderItemSerializer, OrderSerializer, ProductSerializer, CategorySerializer, CategoryProductSerializer, SliderSerializer
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+
+
+
+class SliderViewSet(viewsets.ModelViewSet):
+    queryset = Slider.objects.all()
+    serializer_class = SliderSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryProductViewSet(viewsets.ModelViewSet):
+    queryset = CategoryProduct.objects.all()
+    serializer_class = CategoryProductSerializer
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    
